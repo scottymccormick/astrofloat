ac4e8e0efa3e12a54cd23ab62ca0d07d55e5a190
Scott McCormick <scott.t.mccormick@gmail.com>
2019-01-07 14:08:50 -0800

    Initial action buttons created

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 
+let game;
+
 class Game {
   constructor(){
-    
+    this.round = 1;
+    console.log('Game constructed');
   }
 } 
 
 /*----- cached element references -----*/
+
+const content = document.querySelector('#content');
+const initActions = document.createElement('section');
+
 /*----- event listeners -----*/
 /*----- functions -----*/
 
@@ -15,4 +22,25 @@ init();
 
 function init() {
   console.log('Astrofloat initializted');
+
+  // Add first buttons
+  const startBtn = document.createElement('button');
+  startBtn.textContent = 'Start Game';
+  startBtn.addEventListener('click', newGame);
+  initActions.appendChild(startBtn);
+
+  const howToBtn = document.createElement('button');
+  howToBtn.textContent = 'How to Play';
+  initActions.appendChild(howToBtn);
+
+  initActions.classList.add('initial-actions')
+  content.appendChild(initActions);
+
+}
+
+function newGame() {
+  console.log('New game started');
+  game = new Game();
+
+  if (!initActions.classList.contains('hidden')) initActions.classList.add('hidden');
 }
